c0468a1a4a36b3b9ebfd0fc01a2338e8d540b86c
Konstantinos K <koufogiannakiskon@gmail.com>
2023-01-26 11:25:40 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,12 @@
+const allRecipesData = [
+  { id: 0, name: "Souvlaki", img: "./img/souvlaki.jpg" },
+  { id: 1, name: "Calamari", img: "./img/calamari.jpg" },
+  { id: 2, name: "Cheeseburger", img: "./img/cheeseburger.jpg" },
+  { id: 3, name: "Carbonara", img: "./img/carbonara.jpg" },
+  { id: 4, name: "Pizza", img: "./img/pizza.jpg" },
+  { id: 5, name: "Mousakas", img: "./img/mousakas.jpg" },
+  { id: 6, name: "Fasolada", img: "./img/fasolada.jpg" },
+  { id: 7, name: "Xoxloi", img: "./img/xoxloi.jpg" },
+];
+
+export default allRecipesData;
